d4874106164e24cb464203c828c383a13f5909f5
James McMillan <jmcmillan29@gmail.com>
2018-01-03 18:09:08 -0800

    started adding the inquirer functions to the js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,10 +22,60 @@ connection.connect(function(err) {
 });
 
 let start = function() {
-    console.log("SQL Connection Established")
+    //console.log("SQL Connection Established")
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        console.log(res)
+        for (let i = 0; i < res.length; i++) {
+            console.log(" - - - - - - - - - - - - - - - ");
+            console.log("item number: " + res[i].item_id);
+            console.log("item: " + res[i].product_name);
+            console.log("price: $" + res[i].price);
+            //console.log(" - - - - - - - - - - - - - - - ")
+        }
         connection.end();
     });
+};
+
+// validateInput makes sure that the user is supplying only positive integers for their inputs
+function validateInput(value) {
+    var integer = Number.isInteger(parseFloat(value));
+    var sign = Math.sign(value);
+
+    if (integer && (sign === 1)) {
+        return true;
+    } else {
+        return 'Please enter a whole non-zero number.';
+    }
+}
+
+function purchase() {
+    inquirer.prompt([{
+                type: "input",
+                name: "item_id",
+                message: "Select the item you would like to purchase by item number.",
+                validate: validateInput,
+                filter: Number
+            },
+            {
+                type: "input",
+                name: "quantity",
+                message: "How many of this item would you like to purchase?",
+                validate: validateInput,
+                filter: Number
+            }
+        ])
+        .then(function(purchase) {
+            let item = purchase.item_id;
+            let quantity = purchase.quantity;
+
+            let queryStr = 'SELECT * FROM products WHERE ?';
+
+            connection.query(queryStr, { item_id: item }, function(err, data) {
+                if (err) throw err;
+
+
+            })
+
+
+        });
 };
